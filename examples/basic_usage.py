515525de948791d9ac168400de52488a60df0e02
legriamax <legriamax@proton.me>
2022-04-09 21:44:34 +0000

    create folder

--- examples/basic_usage.py
+++ examples/basic_usage.py
@@ -4,4 +4,28 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#    ht
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import grpc
+import video_streaming_pb2_grpc, video_streaming_pb2
+import argparse
+
+
+def send_list_stream_request(stub):
+    """ Create a list of streams request object """
+
+
+    stream_request = video_streaming_pb2.ListStreamRequest()   
+    responses = stub.ListStreams(stream_request)
+    for stream_resp in responses:
+        yield stream_resp
+
+
+def gen_ima
