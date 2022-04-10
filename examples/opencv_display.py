df73ab34f6fc541c2e3a4ad494443f56418629af
legriamax <legriamax@proton.me>
2022-04-10 09:35:11 +0000

    ARM: 7668/1: fix memset-related crashes caused by recent GCC (4.7.2) optimizations

--- examples/opencv_display.py
+++ examples/opencv_display.py
@@ -1,3 +1,4 @@
+
 # Copyright 2020 Wearless Tech Inc All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -16,17 +17,18 @@
 import grpc
 import video_streaming_pb2_grpc, video_streaming_pb2
 import argparse
+import cv2
+import numpy as np
+import time
+import os
 
+def gen_buffer_probe_request(device_name):
+    """ Create GRPC request to get in memory probe info """
 
-def send_list_stream_request(stub):
-    """ Create a list of streams request object """
+    req = video_streaming_pb2.VideoProbeRequest()
+    req.device_id = device_name
 
-
-    stream_request = video_streaming_pb2.ListStreamRequest()   
-    responses = stub.ListStreams(stream_request)
-    for stream_resp in responses:
-        yield stream_resp
-
+    return req
 
 def gen_image_request(device_name, keyframe_only):
     """ Create an object to request a video frame """
@@ -37,27 +39,32 @@ def gen_image_request(device_name, keyframe_only):
     req.key_frame_only = keyframe_only
     return req
 
-
 if __name__ == "__main__":
-    # Initialize parser 
-    parser = argparse.ArgumentParser(description='Chrysalis Edge Proxy Basic Example')
-    parser.add_argument("--list", action='store_true')
-    parser.add_argument("--device", type=str, default=None, required=False)
-
-    args = parser.parse_args()
     
+    parser = argparse.ArgumentParser(description='Chrysalis Edge Proxy Basic Example')
+    parser.add_argument("--device", type=str, default=None, required=True)
+    parser.add_argument("--keyframe", action='store_true')
+    args = parser.parse_args()
+
     # grpc connection to video-edge-ai-proxy
     options = [('grpc.max_receive_message_length', 50 * 1024 * 1024)]
     channel = grpc.insecure_channel('127.0.0.1:50001', options=options)
     stub = video_streaming_pb2_grpc.ImageStub(channel)
 
-    if args.list:
-        list_streams = send_list_stream_request(stub)
-        for stream in list_streams:
-            print(stream)
+    # displaying video info (if exists)
+    probe = stub.VideoProbe(gen_buffer_probe_request(device_name=args.device))
+    aproxFps = 30
+    if probe.buffer:
+        if probe.buffer.approximate_fps > 0:
+            aproxFps = probe.buffer.approximate_fps
+
+    print(probe)
     
-    if args.device:
-        frame = stub.VideoLatestImage(gen_image_request(device_name=args.device,keyframe_only=False))
-        print("is keyframe: ", frame.is_keyframe)
-        print("frame type: ", frame.frame_type)
-        print("frame shape: ", frame.shape)
+    # requesting video frames
+    req = gen_image_request(device_name=args.device,keyframe_only=args.keyframe)
+    while True:
+        frame = stub.VideoLatestImage(req)
+        # read raw frame data and convert to numpy array
+        if frame is not None:
+            img_bytes = frame.data 
+            re_img = np.frombuffer(img_bytes, dtype=np.uint8)
