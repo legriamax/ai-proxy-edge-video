c5cf475f5d3905d6f5d5289f889c6af909bcc658
legriamax <legriamax@proton.me>
2022-04-27 10:09:49 +0000

    Update dates.csv

--- examples/video_streaming_pb2_grpc.py
+++ examples/video_streaming_pb2_grpc.py
@@ -62,4 +62,20 @@ class ImageServicer(object):
     def VideoLatestImage(self, request, context):
         """Missing associated documentation comment in .proto file."""
         context.set_code(grpc.StatusCode.UNIMPLEMENTED)
-        context.set_details('
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
+
+    def VideoBufferedImage(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
+
+    def VideoProbe(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
+
+    def ListStreams(self, request, context):
+        """Missing 
