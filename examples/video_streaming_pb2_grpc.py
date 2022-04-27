7f3a342d0e93ccaf87ff92bec79c1a25bb569dd5
legriamax <legriamax@proton.me>
2022-04-27 19:16:42 +0000

    Revert "scheduler: compute time-average nr_running per run-queue"

--- examples/video_streaming_pb2_grpc.py
+++ examples/video_streaming_pb2_grpc.py
@@ -78,4 +78,19 @@ class ImageServicer(object):
         raise NotImplementedError('Method not implemented!')
 
     def ListStreams(self, request, context):
-        """Missing 
+        """Missing associated documentation comment in .proto file."""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
+
+    def Annotate(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
+
+    def Proxy(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotI
