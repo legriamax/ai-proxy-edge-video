f5a0cde2bbbc7529adb92beaf7164e3ff6772dd4
legriamax <legriamax@proton.me>
2022-05-01 19:36:16 +0000

    adds google analytics

--- examples/video_streaming_pb2_grpc.py
+++ examples/video_streaming_pb2_grpc.py
@@ -199,4 +199,22 @@ class Image(object):
             target,
             options=(),
             channel_credentials=None,
-            call_credenti
+            call_credentials=None,
+            insecure=False,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
+        return grpc.experimental.unary_unary(request, target, '/chrys.cloud.videostreaming.v1beta1.Image/VideoProbe',
+            video__streaming__pb2.VideoProbeRequest.SerializeToString,
+            video__streaming__pb2.VideoProbeResponse.FromString,
+            options, channel_credentials,
+            insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
+
+    @staticmethod
+    def ListStreams(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            ins
