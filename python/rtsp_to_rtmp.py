16e47fcb1418a310a29e26bb4d7becbd04d87460
legriamax <legriamax@proton.me>
2022-06-30 14:43:11 +0000

    trivial: style

--- python/rtsp_to_rtmp.py
+++ python/rtsp_to_rtmp.py
@@ -80,4 +80,14 @@ class RTSPtoRTMP(threading.Thread):
 
         while True:
             try:
-   
+                options = {'rtsp_transport': 'tcp','stimeout': '5000000', 'max_delay': '5000000', 'use_wallclock_as_timestamps':"1", "fflags":"+genpts", 'acodec':'aac'}
+                self.in_container = av.open(self.rtsp_endpoint, options=options)
+                self.in_video_stream = self.in_container.streams.video[0]
+                self.in_audio_stream = None
+                if len(self.in_container.streams.audio) > 0:
+                    for c in self.in_container.streams.audio:
+                        print(c)
+                    # self.in_audio_stream = self.in_container.streams.audio[0]
+
+                # set codec context in redis
+                setCodecInfo(self.redis_conn, self.in_conta
