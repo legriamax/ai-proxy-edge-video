de076784aec9b8ea9b0a22db11af17cb64a2a974
legriamax <legriamax@proton.me>
2023-04-17 06:33:47 +0000

    rev update

---
+++ server/api/rtsp_process.go
@@ -0,0 +1,41 @@
+
+// Copyright 2020 Wearless Tech Inc All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package api
+
+import (
+	"crypto/md5"
+	"fmt"
+	"net/http"
+	"strings"
+
+	g "github.com/chryscloud/video-edge-ai-proxy/globals"
+	"github.com/chryscloud/video-edge-ai-proxy/models"
+	"github.com/chryscloud/video-edge-ai-proxy/services"
+	"github.com/chryscloud/video-edge-ai-proxy/utils"
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-redis/redis/v7"
+)
+
+type rtspProcessHandler struct {
+	processManager  *services.ProcessManager
+	settingsManager *services.SettingsManager
+	rdb             *redis.Client
+}
+
+func NewRTSPProcessHandler(rdb *redis.Client, processManager *services.ProcessManager, settingsManager *services.SettingsManager) *rtspProcessHandler {
+	return &rtspProcessHandler{
+		processManager:  processManager,
