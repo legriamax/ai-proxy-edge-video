516f493046c675332283906e565562e1c825c03d
legriamax <legriamax@proton.me>
2023-04-18 05:28:43 +0000

    x86, espfix: Make espfix64 a Kconfig option, fix UML

--- server/api/test.go
+++ server/api/test.go
@@ -21,3 +21,25 @@ func NewTestApiHandler(rdb *redis.Client) *testApiHandler {
 }
 
 // TestMqttDeviceStatus Testing mqtt device status
+// @Security ApiKeyAuth
+// @Summary Testing mqtt device status
+// @Description Sends a mqtt message regarding device status
+// @Tags MQTT
+// @Param deviceId query string true "device name/id"
+// @Success 200
+// @Failure 417 {object} api.JSONError "test failed"
+// @Accept json
+// @Produce json
+// @Router /testmqtt/api/v1/devicestatus [get]
+func (th *testApiHandler) TestMqttDeviceStatus(c *gin.Context) {
+	deviceId := c.Query("deviceId")
+
+	processType := models.ProcessTypeRTSP
+	err := utils.PublishToRedis(th.rdb, deviceId, models.MQTTProcessOperation(models.DeviceInternalTesting), processType, nil)
+	if err != nil {
+		g.Log.Error("test mqtt device status failed", err)
+		AbortWithError(c, http.StatusExpectationFailed, err.Error())
+		return
+	}
+
+	c.Stat
