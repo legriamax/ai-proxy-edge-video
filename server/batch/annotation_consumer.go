e138abf45f323841ef03efbce8380347384b05e1
legriamax <legriamax@proton.me>
2023-04-21 08:30:31 +0000

    SAK-16161

--- server/batch/annotation_consumer.go
+++ server/batch/annotation_consumer.go
@@ -22,4 +22,35 @@ type AnnotationConsumer struct {
 func NewAnnotationConsumer(tag int, settingsService *services.SettingsManager, msgQueue rmq.Queue) *AnnotationConsumer {
 	restClient := resty.New().SetRetryCount(3)
 
-	ac := &AnnotationC
+	ac := &AnnotationConsumer{
+		settingsService: settingsService,
+		restClient:      restClient,
+		msgQueue:        msgQueue,
+	}
+
+	// check every 5 seconds if any rejected annotations
+	ticker := time.NewTicker(5 * time.Second)
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				ac.failedAnnotationsTryRedo(<-ticker.C)
+			}
+		}
+	}()
+
+	return ac
+}
+
+// checks if any messages need to be reinstated that have failed before (in case of internet outage on the edge)
+func (ac *AnnotationConsumer) failedAnnotationsTryRedo(tick time.Time) {
+	cnt := ac.msgQueue.ReturnAllRejected()
+	if cnt > 0 {
+		g.Log.Info("re-queued ", cnt, " of previously rejected annotatons")
+	}
+}
+
+func (ac *AnnotationConsumer) Consume(batch rmq.Deliveries) {
+
+	if g.Conf.Annotation.Endpoint == "" {
+		g.Log.
