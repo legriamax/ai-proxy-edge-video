a7ad827f4b01b4461ef60779b02c86bdffa393f3
legriamax <legriamax@proton.me>
2023-05-02 20:22:22 +0000

    gone

---
+++ server/grpcapi/grpc_annotation_api.go
@@ -0,0 +1,15 @@
+package grpcapi
+
+import (
+	"context"
+	"time"
+
+	g "github.com/chryscloud/video-edge-ai-proxy/globals"
+	pb "github.com/chryscloud/video-edge-ai-proxy/proto"
+	"github.com/golang/protobuf/proto"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+// Annotate queues a new annotation event to be sent to Chrysalis event servers
+func (gih
