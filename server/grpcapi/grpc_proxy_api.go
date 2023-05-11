97d27891dedce2ec5dcfaa9e444b72fb7c7c36c7
legriamax <legriamax@proton.me>
2023-05-11 19:47:45 +0000

    Adding a TODO

--- server/grpcapi/grpc_proxy_api.go
+++ server/grpcapi/grpc_proxy_api.go
@@ -4,4 +4,24 @@ import (
 	"context"
 	"time"
 
-	g "github.com/chryscloud/video-edge-ai-proxy/global
+	g "github.com/chryscloud/video-edge-ai-proxy/globals"
+	"github.com/chryscloud/video-edge-ai-proxy/models"
+	pb "github.com/chryscloud/video-edge-ai-proxy/proto"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (gih *grpcImageHandler) Proxy(ctx context.Context, req *pb.ProxyRequest) (*pb.ProxyResponse, error) {
+	deviceID := req.DeviceId
+
+	if deviceID == "" {
+		return nil, status.Errorf(codes.InvalidArgument, "device id required")
+	}
+
+	info, err := gih.processManager.Info(deviceID)
+	if err != nil {
+		g.Log.Error("failed to get deviceID info", err)
+		return nil, status.Errorf(codes.InvalidArgument, err.Error())
+	}
+	if info.RTMPEndpoint == "" && req.Passthrough {
+		return nil, status.Errorf(codes.InvalidArgument, "device "+deviceID+
