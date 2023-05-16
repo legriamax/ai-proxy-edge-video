f266c18c5d0fe5f31e05a40a73abc1782cd38c7e
legriamax <legriamax@proton.me>
2023-05-16 10:29:30 +0000

    Init

--- server/grpcapi/grpc_storage_api.go
+++ server/grpcapi/grpc_storage_api.go
@@ -16,4 +16,26 @@ import (
 )
 
 // Storage - enable disable storage on Chrysalis Cloud
-func (gih *grpcImageHandler) Storage(ctx context.Context, req *pb.StorageRequest) (*pb.Sto
+func (gih *grpcImageHandler) Storage(ctx context.Context, req *pb.StorageRequest) (*pb.StorageResponse, error) {
+
+	deviceID := req.DeviceId
+
+	if deviceID == "" {
+		return nil, status.Errorf(codes.InvalidArgument, "device id required")
+	}
+
+	info, err := gih.processManager.Info(deviceID)
+	if err != nil {
+		g.Log.Error("failed to get deviceID info", err)
+		return nil, status.Errorf(codes.InvalidArgument, err.Error())
+	}
+	if info.RTMPEndpoint == "" {
+		return nil, status.Errorf(codes.InvalidArgument, "device "+deviceID+" doesn't have an associated RTMP stream")
+	}
+
+	apiErr := gih.enableDisableStorageAPICall(req.Start, info.RTMPEndpoint)
+	if apiErr != nil {
+		if apiErr == models.ErrForbidden {
+			return nil, status.Errorf(codes.PermissionDenied, "permission denied")
+		}
+		return nil, status.Errorf(codes.InvalidArgument, fmt.Sprintf("cannot enable
