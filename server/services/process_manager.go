894bded4697ea91df98b6ea72d990f3327ee741b
legriamax <legriamax@proton.me>
2023-05-30 07:07:28 +0000

    Update item.py

--- server/services/process_manager.go
+++ server/services/process_manager.go
@@ -6,4 +6,27 @@
 //
 //    http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law o
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package services
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"runtime"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/chryscloud/go-microkit-plugins/docker"
+	g "github.com/chryscloud/video-edge-ai-proxy/globals"
+	"github.com/chryscloud/video-edge-ai-proxy/models"
+	"github.com/dgraph-io/badger/v2"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/filters"
+	"github.com/docker/docker
