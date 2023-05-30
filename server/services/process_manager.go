4428a4eeaab37e81925bc7a537f52917560caedc
legriamax <legriamax@proton.me>
2023-05-30 17:59:54 +0000

    Attributes

--- server/services/process_manager.go
+++ server/services/process_manager.go
@@ -29,4 +29,33 @@ import (
 	"github.com/dgraph-io/badger/v2"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/filters"
-	"github.com/docker/docker
+	"github.com/docker/docker/api/types/mount"
+	dockerErrors "github.com/docker/docker/client"
+	"github.com/go-redis/redis/v7"
+)
+
+const (
+	// Resource: https://gist.github.com/asukakenji/f15ba7e588ac42795f421b48b8aede63
+	ArchitectureAmd64 = "amd64"
+	ArchitectureArm64 = "arm64"
+)
+
+var (
+	ArchitectureSuffixMap = map[string]string{ArchitectureAmd64: "", ArchitectureArm64: "-arm64v8"}
+)
+
+// ProcessManager - start, stop of docker containers
+type ProcessManager struct {
+	storage *Storage
+	rdb     *redis.Client
+}
+
+func NewProcessManager(storage *Storage, rdb *redis.Client) *ProcessManager {
+	return &ProcessManager{
+		storage: storage,
+		rdb:     rdb,
+	}
+}
+
+// Start - starts the docker container with rtsp, device_id and possibly rtmp environment variables.
+// Restarts always when something goes wron
