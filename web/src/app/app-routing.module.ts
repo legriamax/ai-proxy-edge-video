855bc5777075f77f4b9d09865f88a77f99cf787e
legriamax <legriamax@proton.me>
2023-08-05 07:42:35 +0000

    fix markdown formatting

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,2 +1,14 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } 
+import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProcessesComponent } from './components/processes/processes.component';
+import { ProcessDetailsComponent } from './components/process-details/process-details.component';
+import { ProcessAddComponent } from './components/process-add/process-add.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { SetupComponent } from './components/setup/setup.component';
+import { AppAddComponent } from './components/app-add/app-add.component';
+import { AppDetailsComponent } from './components/app-details/app-details.component';
+
+
+const routes: Routes = [
+  {path: 
