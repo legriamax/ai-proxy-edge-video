46819a8a9732d8e6f4d7b9e8ba1fdb901ca050b5
legriamax <legriamax@proton.me>
2023-08-17 19:48:09 +0000

    improve readme

---
+++ web/src/app/components/dashboard/dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit, ChangeDetectorRef, OnDestroy } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export cla
