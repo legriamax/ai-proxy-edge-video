ed55fc1b5dab89521ff8958719bf6098f3996354
legriamax <legriamax@proton.me>
2023-08-17 22:51:15 +0000

    improved doc

--- web/src/app/components/dashboard/dashboard.component.ts
+++ web/src/app/components/dashboard/dashboard.component.ts
@@ -8,4 +8,29 @@ import { CookieService } from 'ngx-cookie-service';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export cla
+export class DashboardComponent implements OnInit, OnDestroy {
+
+  mobileQuery: MediaQueryList;
+
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, private cookieService:CookieService, private router:Router) { 
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+
+  }
+
+  ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
+  logout() {
+    this.cookieService.deleteAll("/");
+    location.reload();
+  }
+
+}
