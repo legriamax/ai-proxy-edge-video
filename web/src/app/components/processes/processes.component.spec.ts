65918ac6436dab5e5fd7fa7713880e6bb1e9eaca
legriamax <legriamax@proton.me>
2023-09-13 11:09:04 +0000

    Fix CNAME

---
+++ web/src/app/components/processes/processes.component.spec.ts
@@ -0,0 +1,26 @@
+
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProcessesComponent } from './processes.component';
+
+describe('ProcessesComponent', () => {
+  let component: ProcessesComponent;
+  let fixture: ComponentFixture<ProcessesComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ProcessesComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProcessesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
