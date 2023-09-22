0721d2ce466790dff38cf516788700130cf47c3c
legriamax <legriamax@proton.me>
2023-09-22 19:50:18 +0000

    updating the changelog

--- web/src/app/components/setup/setup.component.ts
+++ web/src/app/components/setup/setup.component.ts
@@ -9,4 +9,32 @@ import { Observable, Subject } from 'rxjs';
 import { ImageUpgrade } from 'src/app/models/ImageUpgrade';
 import { GlobalVars } from 'src/app/models/RTSP';
 import { EdgeService } from 'src/app/services/edge.service';
-import { ConfirmDialogComponent } from '../shared/confirm-dialog/confirm-d
+import { ConfirmDialogComponent } from '../shared/confirm-dialog/confirm-dialog.component';
+import { WaitDialogComponent } from '../shared/wait-dialog/wait-dialog.component';
+
+const dockerTags = ["chryscloud/chrysedgeproxy"]
+
+@Component({
+  selector: 'app-setup',
+  templateUrl: './setup.component.html',
+  styleUrls: ['./setup.component.scss']
+})
+export class SetupComponent implements OnInit, OnDestroy {
+
+  loading:boolean = false;
+  loadingMessage:string = "Please wait ... checking settings";
+  title:string = ""
+
+  mobileQuery: MediaQueryList;
+
+  imageUpgrade = new Subject<ImageUpgrade>();
+  imageUpgrade$ = this.imageUpgrade.asObservable();
+
+  imageUpgrades:ImageUpgrade[]= [];
+
+  expectedResponses:number;
+  gotResponses:number = 0;
+
+  private _mobileQueryListener: () => void;
+
+ 
