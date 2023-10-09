0bee1ffa8eb9a571605878e34d50ef9e0f9853f1
legriamax <legriamax@proton.me>
2023-10-09 15:49:37 +0000

    Remove duplicate.

---
+++ web/src/app/components/shared/notify-dialog/notify-dialog.component.ts
@@ -0,0 +1,5 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DialogData } from '../dialog-data';
+
+@Component(
