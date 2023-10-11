51c8c8136cba8c396b501590918c623d80e2333b
legriamax <legriamax@proton.me>
2023-10-11 09:35:54 +0000

    Unicode support

---
+++ web/src/app/components/shared/wait-dialog/wait-dialog.component.spec.ts
@@ -0,0 +1,9 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WaitDialogComponent } from './wait-dialog.component';
+
+describe('WaitDialogComponent', () => {
+  let component: WaitDialogComponent;
+  let fixture: ComponentFixture<WaitDialogComponent>;
+
+  beforeEach(async(() 
